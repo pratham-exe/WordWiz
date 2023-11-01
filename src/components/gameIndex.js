72206fe31111fe60279e1c36389bc4bb77db74b5
Pratham Rao U N <prathamrao001@gmail.com>
2023-11-01 16:42:03 +0530

    Delete and Enter done

--- src/components/gameIndex.js
+++ src/components/gameIndex.js
@@ -1,20 +1,45 @@
 const keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','Enter','Delete'];
 
-var count = 1;
-var test;
+var count = 1, test, fiveCount = 1, keep = 0;
+const left_player = ['w','o','r','l','d'];
+const right_player = ["truth"];
 
 document.addEventListener("keypress", (event) => {
-  console.log(event);
   test = document.getElementById(count);
-  if (count === 50) {
-    count = 0;
+  if (count % 5 === 0) {
+    if (keys.includes(event.key.toLowerCase())) {
+      test.textContent = event.key.toUpperCase();
+      fiveCount = count;
+    }
   }
-  if (event.key === "Enter" || event.key === "Delete") {
+  if (event.key === "Enter" || event.key === "Delete" || count % 5 === 0) {
     keyAnimate(event.key);
-    if (event.key === "Delete") {
-      if (count === 1) {
+    if (event.key === "Enter") {
+      if (count % 5 === 0) {
+        let check = [], correct = 0;
+        for (let i=1;i<=5;i++) {
+          let letter = document.getElementById(i+keep).innerHTML;
+          check.push(letter);
+        }
+        for (let i=0;i<5;i++) {
+          if (left_player[i] === check[i].toLowerCase()) {
+            correct++;
+          }
+        }
+        keep = keep + 5;
+        count++;
+        fiveCount = 1;
+      }
+    } else if (event.key === "Delete") {
+      if ((count - 1) % 5 === 0) {
+        count = count;
+      } else if (count === 1) {
         test.textContent = "";
         count = 1;
+      } else if (fiveCount % 5 === 0) {
+        test = document.getElementById(fiveCount);
+        test.textContent = "";
+        fiveCount = 1;
       } else {
         test = document.getElementById(--count);
         test.textContent = "";
